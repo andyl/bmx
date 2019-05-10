d0296750ef2a3db8c5e54b6d67d8e0b36e273312
andyl <andy@r210.com>
2019-05-10 07:50:40 -0700

    Add DataModel

---
+++ apps/bmx01/lib/bmx01/dm/tracker.ex
@@ -0,0 +1,23 @@
+defmodule Bmx01.Dm.Tracker do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "trackers" do
+    field :type, :string
+    field :uuid, :string
+    field :name, :string
+    field :exid, :string
+    timestamps()
+  end
+
+  def changeset(tracker, attrs) do
+    required_fields = [:type, :name, :exid]
+    optional_fields = [:uuid]
+
+    tracker
+    |> cast(attrs, required_fields ++ optional_fields)
+    |> validate_required(required_fields)
+    |> unique_constraint(:uuid)
+    |> unique_constraint(:exid)
+  end
+end

545651fce336b3086f8fe669f7183517b2289e44
andyl <andy@r210.com>
2019-05-20 11:34:06 -0700

    Add ex_guard

--- mix.exs
+++ mix.exs
@@ -9,12 +9,12 @@ defmodule Bmx.Umbrella.MixProject do
     ]
   end
 
-  # Dependencies listed here are available only for this project
-  # and cannot be accessed from applications inside the apps folder
-  #
   # hex - {:mydep, "~> 0.3.0"}
   # git - {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
   defp deps do
-    [{:mix_test_watch, "~> 0.8", only: :dev, runtime: false}]
+    [
+      {:mix_test_watch, "~> 0.8", only: :dev, runtime: false},
+      {:ex_guard, "~> 1.3", only: :dev}
+    ]
   end
 end

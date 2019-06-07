7953b205b0aef9bfeda1a0eaa5e1dfdc5855e164
andyl <andy@r210.com>
2019-06-07 10:05:42 -0700

    Update mix

--- mix.exs
+++ mix.exs
@@ -15,11 +15,16 @@ defmodule Bmx.Umbrella.MixProject do
 
   defp deps do
     [
-      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
-      {:mix_test_watch, "~> 0.8", only: :dev, runtime: false},
+      # releases
       {:distillery, "~> 2.0"},
+      # static analyzers
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
+      # development and testing
       {:recon_ex, "~> 0.9"},
-      {:ex_guard, "~> 1.3", only: :dev}
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:ex_guard, "~> 1.3", only: :dev, runtime: false},
+      {:mix_test_watch, "~> 0.8", only: :dev, runtime: false},
     ]
   end
 

77b04f757d08e982852b6992b3765adbc4524c86
andyl <andy@r210.com>
2019-06-19 15:45:00 -0700

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -17,14 +17,16 @@ defmodule Bmx.Umbrella.MixProject do
     [
       # releases
       {:distillery, "~> 2.0"},
+      # monitoring and tracing
+      {:recon_ex, "~> 0.9"},
       # static analyzers
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
       # development and testing
-      {:recon_ex, "~> 0.9"},
+      {:ex_todo, "~> 0.1.0", only: :dev, runtime: false},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:ex_guard, "~> 1.3", only: :dev, runtime: false},
-      {:mix_test_watch, "~> 0.8", only: :dev, runtime: false},
+      {:mix_test_watch, "~> 0.8", only: :dev, runtime: false}
     ]
   end
 
@@ -37,7 +39,7 @@ defmodule Bmx.Umbrella.MixProject do
       extras: [
         "README.md",
         "guides/intro.md"
-      ],
+      ]
     ]
   end
 end
